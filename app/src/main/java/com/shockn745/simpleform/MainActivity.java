2ef0b4cfd75f3a2ff8eeb2f22cf5ddc17a7bd7dc
Shock_N745 <Shockn745@gmail.com>
2015-08-05 12:28:55 +0200

    Initial commit

---
+++ app/src/main/java/com/shockn745/simpleform/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shockn745.simpleform;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
